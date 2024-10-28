5865fb99cc1b52f8d675756f77f8bffb6834ea77
Adriano Saadeh <adrianosaadeh@gmail.com>
2024-10-28 19:28:30 -0300

    implement tests

--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -23,5 +23,19 @@ describe('Input Fields', () => {
         cy.get('input[placeholder="John Doe"]')
             .clear()
             .type('Adriano Master')
+
+        cy.get('input[type="email"]').type('adrianolegal@gmail.com')
+        // ou pelo name 
+        cy.get('input[name="email"]')
+            .clear()
+            .type('superdrico@gmail.com')
+
+        cy.get('input[type="number"]').type('12345')
+        // ou pelo name 
+        cy.get('input[name="number"]')
+            .clear()
+            .type('66666')
+
+        cy.get('input[type="date"]').type('2024-12-01')
     })
 })
